442627c54af38a1f3889308afe59550bdedecb32
Jic <lamngockhanhtn@gmail.com>
2018-01-15 08:57:19 +0700

    init project

---
+++ react-native-ethereum/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import './global';
+const Web3 = require('web3');
+
+export default class App extends React.Component {
+
+  componentWillMount() {
+    const web3 = new Web3(
+      new Web3.providers.HttpProvider('https://mainnet.infura.io/')
+    );
+
+    web3.eth.getBlock('latest').then(console.log)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
